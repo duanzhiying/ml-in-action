a519365bf4fbd470596d93a5c055b80ad8e0f7a1
duanzhiying <duanzhiying@sina.cn>
2018-02-25 17:09:08 +0800

    no message

--- test/testSvd.py
+++ test/testSvd.py
@@ -8,6 +8,7 @@ Created on Sun Jan 28 14:05:57 2018
 import numpy as np
 import svd.svd as svd
 
+
 #myMat = np.mat(svd.loadExData())
 #myMat[0,1] = myMat[0,0] = myMat[1,0] = myMat[2,0] = 4
 #myMat[3,3] = 2
